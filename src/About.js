8ec35797b0d85ec129fa78dc282d7d5a7f393a64
ellite07 <shelketushar100@gmail.com>
2022-10-27 10:04:14 +0530

    Homepage=> Service & Trusted component's done

--- src/About.js
+++ src/About.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
 
 const About = () => {
+  const data = {
+    name : "Thapa store",
+    img: "images/hero.jpg"
+  }
+
   return (
-    <div>About</div>
+    <HeroSection myData={data} />
   )
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import HeroSection from './components/HeroSection';
+import Services from './components/Services';
+import Trusted from './components/Trusted';
  
 const Home = () => {
+  const data = {
+    name : "Thapa E-commerce",
+    img: "images/hero.jpg"
+
+  }
   return (
-    <Wrapper className="test">Home</Wrapper>
+    // <Wrapper className="test">Home</Wrapper>
+    <>
+    <HeroSection myData={data}/>
+    <Services/>
+    <Trusted/>
+    </>
   )
 };
 
-const Wrapper = styled.section`
-background-color : ${({theme})=> theme.colors.bg };
-`;
+// const Wrapper = styled.section`
+// background-color : ${({theme})=> theme.colors.bg };
+// `;
 
 export default Home
